d7057f2627ba9fbc7e848b609e1ded07c0ba95f2
RamonRS <celmon1@gmail.com>
2026-04-29 17:04:35 +0200

    DIA5

---
+++ Dia5/src/strings/Ejemplos.java
@@ -0,0 +1,20 @@
+package strings;
+
+public class Ejemplos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String matricula = "0154TGB";
+		
+		//charAt -> Devuelve el caracter que ocupa la posición que se le pasa como parámetro
+		char primerCaracter = matricula.charAt(0);
+		
+		//length() -> Devuelve el número de caracteres
+		int numeroCaracteres = matricula.length();
+		
+		
+		
+	}
+
+}
